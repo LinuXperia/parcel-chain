77eca00392760d41ff36e2ee0e52504f24150ae5
Chris Butcher <c.butcher@hotmail.com>
2017-04-17 17:15:13 -0700

    Added Chain class which will traverse the nodes.

---
+++ src/Chain.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Parcel\Chain;
+
+class Chain implements ChainInterface {
+
+	/**
+	 * List of all the nodes that are attached to this chain.
+	 *
+	 * @var NodeInterface[]
+	 */
+	protected $_Nodes = array();
+
+	/**
+	 * Traverses the nodes in the chain.
+	 *
+	 * @param DispatchInterface $Dispatch
+	 */
+	public function Execute(DispatchInterface $Dispatch) {
+		for ($i = 0; $i < count($this->_Nodes); $i++) {
+
+			$this->_Nodes[$i]->Execute($Dispatch);
+
+			$Dispatch->PersistState();
+
+			if (!$Dispatch->IsPropagating()) {
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Returns all of the nodes for this chain.
+	 *
+	 * @return NodeInterface[]
+	 */
+	public function GetNodes() {
+		return $this->_Nodes;
+	}
+
+	/**
+	 * Link a new node to the end of the chain.
+	 *
+	 * @param NodeInterface $Node
+	 *
+	 * @return $this
+	 */
+	public function LinkNode(NodeInterface $Node) {
+		$this->_Nodes[] = $Node;
+
+		return $this;
+	}
+}
